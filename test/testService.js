2d0832a441b4ef12006fbfb5579966c272e2205a
dumaganr <jrdumagan@gmail.com>
2017-02-23 17:44:42 -0600

    Implemented testing for service.js

---
+++ test/testService.js
@@ -0,0 +1,94 @@
+var db = require('../db.js');
+var service = require('../service.js');
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+describe('Service', function() {
+    describe('newPoll', function() {
+        beforeEach(function() {
+            sinon.stub(db, 'insert').returns(Promise.resolve());
+        });
+
+        afterEach(function() {
+            db.insert.restore();
+            db.getPolls.restore();
+        });
+
+        it('returns pollID 1 on first poll created', function() {
+            var getPollsResult = [];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var q = 'question', o = 'options', ip = '123.456.789.123', expectedValue = 1;
+
+            return service.newPoll(q, o, ip).then(pollID => expect(pollID).to.equal(expectedValue));
+        });
+
+        it('returns pollID 2 on second poll created', function() {
+            var getPollsResult = [{pollID : 1}];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var q = 'question', o = 'options', ip = '123.456.789.123', expectedValue = 2;
+
+            return service.newPoll(q, o, ip).then(pollID => expect(pollID).to.equal(expectedValue));
+        });
+
+        it('increments on additonal polls', function() {
+            var getPollsResult = [{pollID : 10}];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var q = 'question', o = 'options', ip = '123.456.789.123', expectedValue = 11;
+
+            return service.newPoll(q, o, ip).then(pollID => expect(pollID).to.equal(expectedValue));
+        });
+    });
+
+    describe('getPoll', function() {
+        afterEach(function() {
+            db.getPolls.restore();
+        });
+
+        it('returns null when poll ID does not exist', function() {
+            var getPollsResult = [];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var id = 0, expectedValue = null;
+
+            return service.getPoll(id).then(poll => expect(poll).to.equal(expectedValue));
+        });
+
+        it('returns poll with asked poll ID', function() {
+            var getPollsResult = [{pollID : 1, question: "question", options : ["a", "b", "c"], votes : [0, 0, 0], owner : "123.456.789.123"}];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var id = 1, expectedPollID = 1;
+
+            return service.getPoll(id).then(result => expect(result.polls[0].pollID).to.equal(expectedPollID));
+        });
+    });
+
+    describe('getPolls', function() {
+        afterEach(function() {
+            db.getPolls.restore();
+        });
+
+        it('returns null when no polls exist', function() {
+            var getPollsResult = [];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var expectedValue = null;
+
+            return service.getPolls().then(poll => expect(poll).to.equal(expectedValue));
+        });
+
+        it('returns single poll', function() {
+            var getPollsResult = [{pollID : 1, question: "question 1", options : ["a", "b", "c"], votes : [0, 0, 0], owner : "123.456.789.123"}];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var expectedNumPolls = 1;
+
+            return service.getPoll().then(result => expect(result.polls.length).to.equal(expectedNumPolls));
+        });
+
+        it('returns multiple polls', function() {
+            var getPollsResult = [{pollID : 1, question: "question 1", options : ["a", "b", "c"], votes : [0, 0, 0], owner : "123.456.789.123"},
+                                  {pollID : 2, question: "question 2", options : ["d", "e", "f"], votes : [0, 0, 0], owner : "123.456.789.123"}];
+            sinon.stub(db, 'getPolls').returns(Promise.resolve(getPollsResult));
+            var expectedNumPolls = 2;
+
+            return service.getPoll().then(result => expect(result.polls.length).to.equal(expectedNumPolls));
+        });
+    });
+});
