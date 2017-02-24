cfdd21ba57f4b435f0083ae927f7ca6cd2fc6cd6
dumaganr <jrdumagan@gmail.com>
2017-02-24 02:34:52 -0600

    Modified to not call console during testing

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(bodyParser.json());
 //  Calls the service that creates a poll using the information inside the POST (JSON) body
 //
 app.post('/aya/api/create-polls', function(req, res) {
-    console.log('/aya/api/create-polls called');
+    if (process.env.NODE_ENV !==  'test') {
+        console.log('/aya/api/create-polls called');
+    }
     service.newPoll(req.body.questions, req.body.options, req.ip).then(pollID => res.json({"pollID" : pollID}));
 });
 
@@ -25,7 +27,9 @@ app.post('/aya/api/create-polls', function(req, res) {
 //  Calls the service that retrieves a poll via pollID
 //
 app.get('/aya/api/get-polls/:id', function(req, res) {
-    console.log('/aya/api/get-polls/%d called', req.params.id);
+    if (process.env.NODE_ENV !==  'test') {
+        console.log('/aya/api/get-polls/%d called', req.params.id);
+    }
     service.getPoll(req.params.id).then(polls => res.json(polls));
 });
 
@@ -33,7 +37,9 @@ app.get('/aya/api/get-polls/:id', function(req, res) {
 // Calls the service that retrieves all the polls in the database
 //
 app.get('/aya/api/get-polls', function(req, res) {
-    console.log('/aya/api/get-polls called');
+    if (process.env.NODE_ENV !==  'test') {
+        console.log('/aya/api/get-polls called');
+    }
     service.getPolls().then(polls => res.json(polls));
 });
 
@@ -42,6 +48,10 @@ var port = 8080;
 //
 // Starts the web server
 //
-app.listen(port, function() {
-    console.log("Listening to port", port);
+var server = app.listen(port, function() {
+    if (process.env.NODE_ENV !==  'test') {
+        console.log("Listening to port", port);
+    }
 });
+
+module.exports = server;
