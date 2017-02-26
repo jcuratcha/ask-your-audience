b11a38d144ec91c9a97d6a8498f24f996c64147f
jcuratcha <curatchaj@gmail.com>
2017-02-26 11:06:33 -0600

    Remove generated files from repo, since they're generated on build

--- mobile/ask-your-audience/app/shared/person/person.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Person = (function () {
-    function Person() {
-    }
-    return Person;
-}());
-exports.Person = Person;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGVyc29uLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsicGVyc29uLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTtJQUFBO0lBR0EsQ0FBQztJQUFELGFBQUM7QUFBRCxDQUFDLEFBSEQsSUFHQztBQUhZLHdCQUFNIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFBlcnNvbiB7XHJcblx0ZW1haWw6IHN0cmluZztcclxuXHRwYXNzd29yZDogc3RyaW5nO1xyXG59Il19
