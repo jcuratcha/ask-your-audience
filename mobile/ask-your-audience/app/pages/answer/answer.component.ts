40a5aeb721a59496b0e116202464c80b1194e969
jcuratcha <curatchaj@gmail.com>
2017-02-26 21:45:54 -0600

    Add vote method in poll service

--- mobile/ask-your-audience/app/pages/answer/answer.component.ts
+++ mobile/ask-your-audience/app/pages/answer/answer.component.ts
@@ -17,12 +17,14 @@ export class AnswerComponent implements OnInit {
 	question: string;
 	options: Array<string>;
 
+	private id: number;
+
 	constructor(private pollService: PollService, private router: ActivatedRoute, private page: Page) {
 		console.log("Current page: AnswerComponent");
 	}
 
 	ngOnInit() {
-		let id = this.router.snapshot.params['id'];
+		this.id = this.router.snapshot.params['id'];
 		this.pollService.getPoll(id)
 			.subscribe(newPoll => {
 				this.poll = newPoll;
@@ -38,6 +40,6 @@ export class AnswerComponent implements OnInit {
 		var index = this.options.indexOf(votedOption);
 		console.log(index);
 
-		// make request to vote for this item on this poll
+		this.pollService.addPollVote(this.id, index);
 	}
 }	
