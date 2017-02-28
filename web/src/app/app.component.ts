538605a7975b2c85567585c93fcf7c4f8b9091a6
jcuratcha <curatchaj@gmail.com>
2017-02-28 13:21:29 -0600

    Added poll list service and http module

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,35 +1,44 @@
-import { Component } from '@angular/core';
-import{ Poll } from './poll';
+import { Component, OnInit } from '@angular/core';
+import { Poll } from './poll';
 
-const MOCKDB: Poll[] = [
-  { question: "this is a mockDB", 
-    options: ["option 1", "option 2", "option 3", "option 4"],
-    votes: [0, 1, 2, 3],
-    voted: false
-  },
-  { question: "this is the second poll", 
-    options: ["hello", "world"],
-    votes: [0, 1],
-    voted: false
-  },
-  { question: "User cannot vote in this poll as theyve already voted", 
-    options: ["cannot", "vote"],
-    votes: [0, 1],
-    voted: true
-  }
-];
+import { PollListService } from './services/poll-list.service'
+
+// const MOCKDB: Poll[] = [
+//   { question: "this is a mockDB", 
+//     options: ["option 1", "option 2", "option 3", "option 4"],
+//     votes: [0, 1, 2, 3],
+//     voted: false
+//   },
+//   { question: "this is the second poll", 
+//     options: ["hello", "world"],
+//     votes: [0, 1],
+//     voted: false
+//   },
+//   { question: "User cannot vote in this poll as theyve already voted", 
+//     options: ["cannot", "vote"],
+//     votes: [0, 1],
+//     voted: true
+//   }
+// ];
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  providers: [PollListService]
 })
-export class AppComponent  { 
-  name = 'test'; 
-  //polls is an array of poll
-  polls = MOCKDB;
 
-  onNotify($event: Poll){
-    let newPoll: Poll = Object.assign({}, $event);
-    this.polls.unshift(newPoll);
+export class AppComponent implements OnInit { 
+  polls: Array<Poll> = [];
+  name = 'test'; 
+
+  constructor (private pollListService: PollListService) {}
+
+  ngOnInit() {
+    this.pollListService.getAllPolls()
+      .subscribe(loadedPolls => {
+          loadedPolls.forEach((poll: Poll) => {
+          this.polls.unshift(poll);
+        });
+    });
   }
 }
