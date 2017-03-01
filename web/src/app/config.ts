e04c4fd54767cf3df64398408c6398b5ef151016
jcuratcha <curatchaj@gmail.com>
2017-03-01 10:43:57 -0600

    Changed error handling, removed unneeded header

--- web/src/app/config.ts
+++ web/src/app/config.ts
@@ -1,5 +1,6 @@
 export class Config {
-  static apiUrl = "http://ec2-52-60-106-250.ca-central-1.compute.amazonaws.com:8080"; // John
-  // static apiUrl = "http://ec2-54-213-112-197.us-west-2.compute.amazonaws.com:8080"; // Ryan
+  static apiUrl = "http://localhost:8080" // local dev machine
+  // static apiUrl = "http://ec2-52-60-106-250.ca-central-1.compute.amazonaws.com:8080"; // John - Production
+  // static apiUrl = "http://ec2-54-213-112-197.us-west-2.compute.amazonaws.com:8080"; // Ryan - Testing
   static token = "";
 }
