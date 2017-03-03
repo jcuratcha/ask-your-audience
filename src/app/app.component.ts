c854c0dd1687008007dfe1d9985047db233b516e
jcuratcha <curatchaj@gmail.com>
2017-03-03 10:30:13 -0600

    Remove unneeded commented code, fix merge problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,24 +3,6 @@ import { Poll } from './poll';
 
 import { PollListService } from './services/poll-list.service'
 
-// const MOCKDB: Poll[] = [
-//   { question: "this is a mockDB", 
-//     options: ["option 1", "option 2", "option 3", "option 4"],
-//     votes: [0, 1, 2, 3],
-//     voted: false
-//   },
-//   { question: "this is the second poll", 
-//     options: ["hello", "world"],
-//     votes: [0, 1],
-//     voted: false
-//   },
-//   { question: "User cannot vote in this poll as theyve already voted", 
-//     options: ["cannot", "vote"],
-//     votes: [0, 1],
-//     voted: true
-//   }
-// ];
-
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
@@ -31,13 +13,11 @@ export class AppComponent implements OnInit {
   polls: Array<Poll> = [];
   name = 'test'; 
 
-<<<<<<< HEAD
   onNotify($event: Poll){
     let newPoll: Poll = Object.assign({}, $event);
     this.polls.unshift(newPoll);
   }
-=======
->>>>>>> 88cb752... Added poll list service and http module
+  
   constructor (private pollListService: PollListService) {}
 
   ngOnInit() {
