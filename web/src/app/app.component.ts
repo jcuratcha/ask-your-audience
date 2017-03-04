8d40e38cac6653c3ea3f588d3e2289ff7d8522be
John Curatcha <jcuratcha@users.noreply.github.com>
2017-03-04 17:27:28 -0500

    [WIP] Add HTTP Poll List Service (#9)
    * Moved files into web folder
    
    * Added gitignore file to ignore generated files
    
    * Added node_modules to gitignore
    
    * Added more generated files to gitignore
    
    * Added poll list service and http module
    
    * Changed error handling, removed unneeded header
    
    * Move files back out for easy merge
    
    * Moved files into web folder
    
    * Added gitignore file to ignore generated files
    
    * Added node_modules to gitignore
    
    * Added more generated files to gitignore
    
    * Added poll list service and http module
    
    * Changed error handling, removed unneeded header
    
    * Move files back out for easy merge
    
    * Made poll constructor arguments optional
    
    * Updated gitignore, restored missing system config
    
    * Fixed comments in gitignore
    
    * # This is a combination of 4 commits.
    # The first commit's message is:
    
    Added more generated files to gitignore
    
    # The commit message #2 will be skipped:
    
    # Added poll list service and http module
    
    # The commit message #3 will be skipped:
    
    # Changed error handling, removed unneeded header
    
    # The commit message #4 will be skipped:
    
    # Move files back out for easy merge
    
    * # This is a combination of 4 commits.
    # The first commit's message is:
    
    Added more generated files to gitignore
    
    # The commit message #2 will be skipped:
    
    # Added gitignore file to ignore generated files
    
    # The commit message #3 will be skipped:
    
    # Added node_modules to gitignore
    
    # The commit message #4 will be skipped:
    
    # Added more generated files to gitignore
    
    * Added more generated files to gitignore
    
    * Remove unneeded commented code, fix merge problems
    
    * Move files back into web folder
    
    * Merge conflicts
    
    * Added gitignore file to ignore generated files
    
    * Added node_modules to gitignore
    
    * Added more generated files to gitignore
    
    * Added poll list service and http module
    
    * Changed error handling, removed unneeded header
    
    * Move files back out for easy merge
    
    * Moved files into web folder
    
    * Added gitignore file to ignore generated files
    
    * Added more generated files to gitignore
    
    * Added node_modules to gitignore
    
    * Added poll list service and http module
    
    * Changed error handling, removed unneeded header
    
    * Move files back out for easy merge
    
    * Made poll constructor arguments optional
    
    * Updated gitignore, restored missing system config
    
    * Fixed comments in gitignore
    
    * # This is a combination of 4 commits.
    # The first commit's message is:
    
    Added more generated files to gitignore
    
    # The commit message #2 will be skipped:
    
    # Added poll list service and http module
    
    # The commit message #3 will be skipped:
    
    # Changed error handling, removed unneeded header
    
    # The commit message #4 will be skipped:
    
    # Move files back out for easy merge
    
    * # This is a combination of 4 commits.
    # The first commit's message is:
    
    Added more generated files to gitignore
    
    # The commit message #2 will be skipped:
    
    # Added gitignore file to ignore generated files
    
    # The commit message #3 will be skipped:
    
    # Added node_modules to gitignore
    
    # The commit message #4 will be skipped:
    
    # Added more generated files to gitignore
    
    * Added more generated files to gitignore
    
    * Remove unneeded commented code, fix merge problems
    
    * Move files back into web folder

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,35 +1,31 @@
-import { Component } from '@angular/core';
-import{ Poll } from './poll';
+import { Component, OnInit } from '@angular/core';
+import { Poll } from './poll';
 
-const MOCKDB: Poll[] = [
-  { question: "this is a mockDB", 
-    options: ["option 1", "option 2", "option 3", "option 4"],
-    votes: [0, 1, 2, 3],
-    voted: false
-  },
-  { question: "this is the second poll", 
-    options: ["hello", "world"],
-    votes: [0, 1],
-    voted: false
-  },
-  { question: "User cannot vote in this poll as theyve already voted", 
-    options: ["cannot", "vote"],
-    votes: [0, 1],
-    voted: true
-  }
-];
+import { PollListService } from './services/poll-list.service'
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  providers: [PollListService]
 })
-export class AppComponent  { 
+
+export class AppComponent implements OnInit { 
+  polls: Array<Poll> = [];
   name = 'test'; 
-  //polls is an array of poll
-  polls = MOCKDB;
 
   onNotify($event: Poll){
     let newPoll: Poll = Object.assign({}, $event);
     this.polls.unshift(newPoll);
+  }
+  
+  constructor (private pollListService: PollListService) {}
+
+  ngOnInit() {
+    this.pollListService.getAllPolls()
+      .subscribe(loadedPolls => {
+          loadedPolls.forEach((poll: Poll) => {
+          this.polls.unshift(poll);
+        });
+    });
   }
 }
