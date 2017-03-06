813769292d8bd7ba48963ad4794e25ad9d6b6af8
franktby <Frank@Monkey-D-Twisted-MacBook-Air.local>
2017-03-06 15:45:26 -0600

    old-version will commit changes later

--- mobile/ask-your-audience/app/pages/answeredQuestions/answeredQuestions.component.ts
+++ mobile/ask-your-audience/app/pages/answeredQuestions/answeredQuestions.component.ts
@@ -10,38 +10,8 @@ import tabViewModule = require("ui/tab-view");
 	providers: [PollListService]
 })
 
-export class answeredQuestionsComponent implements OnInit {
+export class answeredQuestionsComponent  {
 
-	var items = [];
-var StackLayout0 = new stackLayoutModule.StackLayout();
-var label0 = new labelModule.Label();
-label0.text = "Tab 0";
-StackLayout0.addChild(label0);
-var tabEntry0 = {
-    title: "Tab 0",
-    view: StackLayout0
-};
-items.push(tabEntry0);
-var StackLayout1 = new stackLayoutModule.StackLayout();
-var label1 = new labelModule.Label();
-label1.text = "Tab 1";
-StackLayout1.addChild(label1);
-var tabEntry1 = {
-    title: "Tab 1",
-    view: StackLayout1
-};
-items.push(tabEntry1);
-tabView.items = items;
-tabView.selectedIndex = 9;
 
-  var tabView = new tabViewModule.TabView();
-  tabView.on(tabViewModule.TabView.selectedIndexChangedEvent, (args: tabViewModule.SelectedIndexChangedEventData) => {
-    actualOldIndex = args.oldIndex;
-    actualNewIndex = args.newIndex;
-});
-
-ngOnInit() {
-    
-  }
 
 }
