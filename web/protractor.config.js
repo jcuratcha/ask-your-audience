7011d5d763dbac262f48c8e6465e40e960939228
dumaganr <jrdumagan@gmail.com>
2017-03-14 21:34:22 -0500

    Improved e2e testing setup

--- web/protractor.config.js
+++ web/protractor.config.js
@@ -2,6 +2,7 @@ exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['src/spec/*-spec.js'],
+  directConnect: true,
   capabilities: {
       browserName: 'chrome'
   }
