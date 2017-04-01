f7f77e5c3382462c02bff5861d30d25af19ce4cc
mzielonko <umzielom@myumanitoba.ca>
2017-04-01 04:43:32 -0500

    testing for single-pol.component complete (waiting on Suneet's confirmation of purpose for closePoll()

---
+++ web/src/app/polls/single-poll.component.spec.ts
@@ -0,0 +1,53 @@
+import {Poll} from './../poll';
+import {SinglePollComponent} from './single-poll.component';
+
+describe ('single-poll.component', () => {
+   let testComponent:SinglePollComponent;
+
+   beforeEach( () => {
+      testComponent = new SinglePollComponent();
+   });
+
+   it ('ngOnInit: Overrides any text currently in the shownText field', () => {
+      testComponent.shownText = "a";
+      testComponent.poll = new Poll(123456, "test question", ["Yes", "No"], [152, 180], "test owner");
+      testComponent.poll.question = "b";
+      testComponent.ngOnInit();
+      expect(testComponent.shownText).not.toBe("a");
+   });
+
+   it ('ngOnInit: A null question will also make shownText null', () => {
+      testComponent.shownText = "a";
+      testComponent.poll = new Poll(123456, "test question", ["Yes", "No"], [152, 180], "test owner");
+      testComponent.poll.question = null;
+      testComponent.ngOnInit();
+      expect(testComponent.shownText).toBe(null);
+   });
+
+   it ('toggle: changes from both directions (t-f and f-t)', () => {
+      testComponent.active = true;
+      testComponent.toggle();
+      expect(testComponent.active).toBe(false);
+      testComponent.toggle();
+      expect(testComponent.active).toBe(true);
+   });
+
+   it ('closePoll: changes active to false in any occasion', () => {
+      testComponent.active = true;
+      testComponent.closePoll(false);
+      expect(testComponent.active).toBe(false);
+      testComponent.closePoll(false);
+      expect(testComponent.active).toBe(false);
+
+      testComponent.active = true;
+      testComponent.closePoll(true);
+      expect(testComponent.active).toBe(false);
+      testComponent.closePoll(true);
+      expect(testComponent.active).toBe(false);
+
+   });
+
+   it ('closePoll: find out if Suneet meant to ignore the input of this function', () => {
+      expect(true).toBe(false);
+   })
+})
