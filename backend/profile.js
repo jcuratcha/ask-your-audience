5ff6787def6aa1986da6dc6bf0a472461dd6330d
Richard <camarar@myumanitoba.ca>
2017-04-08 12:47:30 -0500

    Created Profile database schema

---
+++ backend/profile.js
@@ -0,0 +1,17 @@
+//
+// Structure of profile object / schema used inside database
+//
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var profileSchema = new Schema({
+    _id : Number,
+    username : { type: String, required: true, unique: true }
+    password : String,
+    displayName : String,
+    votedPolls : [{ type: Number, ref: 'Poll' }]
+});
+
+module.exports = mongoose.model('Profile', profileSchema);
