6e14bcb753de2043a433b82d92e55354b1451e6f
jcuratcha <curatchaj@gmail.com>
2017-04-09 09:27:04 -0500

    Remove autogenerated files

--- web/src/shim_reflect-metadata.js
+++
@@ -1,7 +0,0 @@
-// Loads module needed for class decorators, for the karma testing suite only
-//    Note: class decorators include @Component declaration describing component.
-//    Without this file, these will not be linked properly
-"use strict";
-require('core-js/es7/reflect');
-require('zone.js/dist/zone');
-//# sourceMappingURL=shim_reflect-metadata.js.map
