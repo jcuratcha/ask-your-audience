03193486fd31b2e990dae38e656ec62a93ae3cdb
jcuratcha <curatchaj@gmail.com>
2017-04-12 16:16:46 -0500

    change mobile to point to production

--- mobile/ask-your-audience/app/shared/config.ts
+++ mobile/ask-your-audience/app/shared/config.ts
@@ -1,8 +1,7 @@
 export class Config {
- //static apiUrl = "http://ec2-52-60-106-250.ca-central-1.compute.amazonaws.com:8080"; // Production
+ static apiUrl = "http://ec2-52-60-106-250.ca-central-1.compute.amazonaws.com:8080"; // Production
   // static apiUrl = "http://ec2-54-213-112-197.us-west-2.compute.amazonaws.com:8080"; // Develop/Staging
   // static apiUrl = "http://ec2-35-161-110-163.us-west-2.compute.amazonaws.com:8080"; // Testing 1
   //  static apiUrl = "http://localhost:8080"; // Dev Machine
-  static apiUrl="http://ec2-35-161-110-163.us-west-2.compute.amazonaws.com:8080";
   static token = "";
 }
